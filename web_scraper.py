948b2c544c237ec30dab6b59432b8a8768a0b3a5
HumzahJavid <25638011+HumzahJavid@users.noreply.github.com>
2019-11-28 13:38:21 +0000

    Initial commit: pulls audio from website
    Using BeautifulSoup and Requests

---
+++ web_scraper.py
@@ -0,0 +1,95 @@
+import urllib.request
+import bs4
+import requests
+from bs4 import BeautifulSoup
+import html
+
+def get_page_urls():
+    base_url = "https://muslimcentral.com/series/yasir-qadhi-seerah/page/"
+    page_urls = []
+    for i in list(range(12))[::-1]:
+        page_urls.append(base_url+str(i))
+    return page_urls
+
+def is_downloadable(url):
+    """
+    Does the url contain a downloadable resource
+    """
+    h = requests.head(url, allow_redirects=True)
+    header = h.headers
+    content_type = header.get('content-type')
+    if 'text' in content_type.lower():
+        return False
+    if 'html' in content_type.lower():
+        return False
+    return True
+
+def get_html_soup(url):
+    r = requests.get(url)
+    if r.status_code == 404:
+        return None
+    else:
+        soup = bs4.BeautifulSoup(r.text, 'html.parser')
+    return soup
+
+def extract_data_elements(soup):
+    # the formatting of the website changed as I was coding this 27/11/19 @ 3-3:30pm
+    #return soup.select("div.ep.ep--narrow > button")
+    return soup.find_all("div", {"itemtype":"http://schema.org/AudioObject"})
+
+def clean_titles(titles, underscore=False):
+    clean_titles = []
+    for i in range(len(titles)):
+        title = html.unescape(titles[i])
+        if underscore:
+            #"_".join(title.split(" "))
+            title.replace(" ", "_")
+        clean_titles.append(title)
+    return clean_titles
+
+def parse_data_elements(soup_data):
+    titles = []
+    urls =[]
+    for i in range(len(soup_data)):
+        title = soup_data[i].meta["content"]
+        url = soup_data[i].a["href"]
+        titles.append(title)
+        urls.append(url)
+    titles=clean_titles(titles, True)
+    
+    return (titles,urls)
+    
+def get_titles_urls(page_urls):
+    video_titles = []
+    video_urls = []
+    for page_url in page_urls:
+        soup = get_html_soup(page_url)
+        element_containing_episodes = extract_data_elements(soup)
+        video_titles,video_urls = parse_data_elements(element_containing_episodes)
+    return video_titles[::-1],video_urls[::-1]
+
+    
+def file_stats(title, url):
+    file_len = int(urllib.request.urlopen(url).headers.get("Content-Length"))
+    file_mb = str(file_len/10.0**6)[:5]
+    print(f"File name: {title}")
+    print(f"File size: {file_mb} MB")
+    print("Downloading...")
+    
+
+def download(file_title, file_url, progress_bar=False):
+
+    if is_downloadable(file_url):
+        file_stats(file_title, file_url)
+        if progress_bar:
+            #tqdm PROGRESS BAR IS SLOW 4:16(m:s) with vs < 0:20 without
+            response = requests.get(file_url, stream=True)
+            with open(file_title+".mp3", "wb") as handle:
+                for data in tqdm.tqdm(response.iter_content()):
+                    handle.write(data)
+        else:
+            urllib.request.urlretrieve(file_url, file_title+".mp3")
+
+page_urls = get_page_urls()
+titles, urls = get_titles_urls([page_urls[0]])
+download(titles[0], urls[0])
